1aab95a146c6dcd89768a603193139f374bf8df1
lgadsden <lgadsden@email.gwu.edu>
2015-04-21 17:39:57 -0400

    final changes
    last changes before group meeting ... changed lowest semester to sample
    to fall 2011, tried using association mining on 2nd portion but did not
    produce good results. Best if students choose a course or courses first.

--- StudentHistoryV21.R
+++ StudentHistoryV21.R
@@ -1,13 +1,14 @@
 #load required packages  
-require(arulesSequences)
-require(tidyr)
-library("stringr")
+library(arulesSequences)
+library(tidyr)
+library(stringr)
 
 ### Create main data sheet to perform analysis(Student Data)
 
 # load CSV course history and course number change list
 student_course_reg_mod <- read.csv("student_course_reg_mod.csv") 
 
+# add course cn variable --- has subject number and course name combined
 student_course_reg_mod$course_cn <- with(student_course_reg_mod,paste(subject_num, course, sep = ": "))
 
 # remove all commas from courses - needed to choose predictions from rhs without errrors
@@ -30,7 +31,7 @@ student_data$reg_code <- regvalues[match(student_data$register_term_desc, regter
 
 ### choose semester for recommendation & a random student
 #select random semester (does not include 1st semester becuase no prior history)
-semester <- sample(2:9,1)
+semester <- sample(3:9,1)
 chosen_semester <- subset(student_data, reg_code == semester)
 
 #select a random student
@@ -40,22 +41,24 @@ student <- subset(student_data, student_unique_identifier == id)
 student$course_cn <- factor(student$course_cn)
 student <- student[order(student$reg_code),]
 
-#make sure student has a prior history 
-spec_student_semrange   <- semester - min(student$reg_code)
+# create list for all courses available for the current semester 
+courses_available <- as.character(unique(chosen_semester$course_cn))
 
+#check if the student has a prior history --- if the student has taken any courses already 
+spec_student_semrange   <- semester - min(student$reg_code)
+spec_student_semrange
+
+#if student has 
 if(spec_student_semrange >= 1) {
     
 #grab all of the student's history prior to the selected semester  
 prior_stud_hist <- subset(student, student$reg_code %in% 1:(semester-1)) 
 
 #extract attributes from the chosen student's current most current semester 
-attributes <- student[nrow(student), c("student_college_group_desc", "student_degree_code", "student_major_desc", "student_minor_code", "student_level_desc")]
+attributes <- student[nrow(student), c("student_college_group_desc", "student_degree_code", "student_major_desc", "student_minor_code", "student_level_desc","student_type_desc")]
 
 #select all student that match the attributes of that student (does not include student)
 student_selection <- subset(student_data,student_unique_identifier!= id & student_college_group_desc == attributes$student_college_group_desc & student_degree_code==attributes$student_degree_code & student_major_desc == attributes$student_major_desc & reg_code %in% 1:(semester-1))
-
-# create list for all courses available for the current semester 
-courses_available <- factor(unique(chosen_semester$course_cn))
 
 # create list of all courses that similar students have taken that are avaible this semester, but remove all of the courses that our student has taken
 courses_available_student <- factor(courses_available[which(courses_available %in% student_selection$course_cn)])
@@ -90,13 +93,13 @@ write.table(student_trans_data, "student_trans.csv", sep="\t", row.names=FALSE, 
 class_basket <- read_baskets("student_trans.csv",sep = "\t",info =  c("sequenceID","eventID"))
 
 #load data into cspade --- used inspect() to see rules 
-student_rules <- cspade(class_basket, parameter = list(support = 0.01, maxsize = 8, maxlen = 4), control = list(verbose = TRUE))
+student_rules <- cspade(class_basket, parameter = list(support = 0.02, maxsize = 4, maxlen = 4), control = list(verbose = TRUE))
 
 #remove all ' "\ ' from item labels so that they can be subset
 itemLabels(student_rules) <- gsub('\"','',itemLabels(student_rules))
 
 # places confidence rules on data and changes class to "sequencerules" which is needed to subset by lhs or rhs sides
-student_rules_info<- ruleInduction(student_rules, confidence = 0.25,control = list(verbose = TRUE))
+student_rules_info<- ruleInduction(student_rules, confidence = 0.3,control = list(verbose = TRUE))
 
 # chooses courses that our students have taken that are part of the rules (LHS or RHS)
 student_course_list <- as.character(unique(prior_stud_hist$course_cn))
@@ -105,41 +108,28 @@ student_list_sub <- which(student_course_list %in% itemLabels(student_rules))
 student_lcourse_list_sub <- which(student_course_lsemester %in% itemLabels(student_rules))
 
 #subsets the lhs so that it at least includes courses that our selected student has already taken  
-#specific_student_rules <- as(subset(student_rules_info, lhs(student_rules_info) %in% student_course_list[student_list_sub]), "data.frame")
 
-lockey <- subset(student_rules_info, lhs(student_rules_info) %in% student_course_lsemester[student_lcourse_list_sub])
-okey <- subset(lockey, !rhs(lockey) %in% student_course_list[student_list_sub])
+sub1 <- subset(student_rules_info, lhs(student_rules_info) %in% student_course_lsemester[student_lcourse_list_sub] & lift > 1.2)
+sub1 <- subset(sub1, !rhs(sub1) %in% student_course_list[student_list_sub])
 
-hops <- as(lockey, "data.frame")
-ice <- as(okey, "data.frame")
+srule_df <- as(sub1, "data.frame")
     
 #makes it so that the lhs and rhs are seprate columns 
-#specific_student_rules <- separate(data = specific_student_rules, col = rule, into = c("lhs", "rhs"), sep ="=>")
-#specific_student_rules <- specific_student_rules[order(specific_student_rules$lift, decreasing = T), ]
+if(nrow(srule_df) > 0){
+    srule_df <- separate(data = srule_df, col = rule, into = c("lhs", "rhs"), sep ="=>")
+    srule_df <- srule_df[with(srule_df, order(-support, -lift)), ]
+} else{srule_df <- 0}
 
-if(nrow(ice) > 0){
-    ice <- separate(data = ice, col = rule, into = c("lhs", "rhs"), sep ="=>")
-    ice <- ice[with(ice, order(-support, -lift)), ]
-} else{ice <- 0}
-
-if(nrow(hops) >0 ){
-    hops <- separate(data = hops, col = rule, into = c("lhs", "rhs"), sep ="=>")
-    hops <- hops[order(hops$lift, decreasing = T), ]
-} else{hops <-0}
-
-if(length(unique(ice$rhs)) > 0 & length(unique(ice$rhs))<= 10){
-    rec <- unique(ice$rhs)[1:length(unique(ice$rhs))]
-} else if(length(unique(ice$rhs) > 5)) {
-    rec <- unique(ice$rhs)[1:10]
-} else { print("Sorry no reccomendation :(")}
+rec <- unique(srule_df$rhs)
 rec <- unlist(str_split(rec,","))
 rec <- gsub(" <{","",rec, perl = TRUE)
 rec <- gsub("}>","",rec, perl = TRUE)
 rec <- rec[!duplicated(rec)]
+rec <- intersect(rec,courses_available)
+
 rec <- if(length(rec) <= 5){rec <- rec
 } else if(length(rec)> 5){
     rec <- rec[1:5]}
-
 
 per_corr <- length(intersect(student[student$reg_code %in% semester,11],rec))/length(student[student$reg_code %in% semester,11])
 per_corr_life <- length(intersect(student[student$reg_code %in% semester:9,11],rec))/length(rec)
@@ -151,4 +141,23 @@ print(intersect(student[student$reg_code %in% semester,11],rec))
 cat("The recommendations were", per_corr, "accurate for this semester.")
 cat("The recommendations were", per_corr_life, "accurate if semesters up to Spring 2014 are included.")
 
-} else{print ("Need to create basket for new students")}
+} else{ 
+#select all student that match the attributes of that student (does not include student)
+attributes <- subset(student, reg_code == semester)[1, c("student_college_group_desc", "student_degree_code", 
+                                                         "student_major_desc", "student_minor_code", "student_level_desc","student_type_desc")]
+#select all student that match the attributes of that student (does not include student)
+student_selection <- subset(student_data,student_unique_identifier!= id & student_college_group_desc == attributes$student_college_group_desc & student_degree_code==attributes$student_degree_code & student_type_desc == attributes$student_type_desc & reg_code %in% 1:(semester-1))
+student_selection$course_cn <- factor(student_selection$course_cn)
+arule_stud_data <- student_selection[,c(1,11)]
+arule_stud_data <- arule_stud_data[!duplicated.data.frame(arule_stud_data),]
+
+aggrData <- split(arule_stud_data[,"course_cn"], arule_stud_data[,"student_unique_identifier"])
+
+st_TrnsData <- as(aggrData, "transactions")
+
+rules = apriori(st_TrnsData, parameter=list(support=0.05, confidence=0.3))
+
+ar_rules <- subset(rules, lift > 1.2)
+ar_rules
+inspect(ar_rules)
+}
